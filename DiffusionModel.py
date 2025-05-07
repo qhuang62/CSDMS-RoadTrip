8dfc6eb70c732e54abb002d4831353522c9829bb
qin <qhuang62.asu.edu>
2025-05-07 23:42:40 +0000

    add stable time step function

--- DiffusionModel.py
+++ DiffusionModel.py
@@ -3,6 +3,11 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
+def calculate_stable_time_step(dx, diffusivity):
+    """calcualte a stable time step for the model"""
+    return 0.5 * dx ** 2 / diffusivity
+    
+
 def plot_profile(x, cake, color="r"):
     """display the cake profile"""
     plt.figure()
@@ -28,7 +33,7 @@ C[x > Lx / 2] = C_right
 plot_profile(x, C)
 
 nt = 5000 
-dt = 0.5 * dx ** 2 / D 
+dt = calculate_stable_time_step(dx, D)
 
 for t in range(0, nt): 
     C[1:-1] += D * dt / dx ** 2 * (C[:-2] - 2 * C[1:-1] + C[2:]) 
