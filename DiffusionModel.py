1db58dd06cef91b44f88855fe6aefe0ca5f9abb0
qin <qhuang62.asu.edu>
2025-05-07 23:30:57 +0000

    add plot_profile function

--- DiffusionModel.py
+++ DiffusionModel.py
@@ -1,58 +1,36 @@
 """One Dimensional Diffusion Model"""
 
-#import libraries
-import numpy as np #for arrays
-import matplotlib.pyplot as plt #for plotting
+import numpy as np 
+import matplotlib.pyplot as plt 
 
-D = 100 #diffusivity
-Lx = 300 #domain size
+def plot_profile(x, cake, color="r"):
+    """display the cake profile"""
+    plt.figure()
+    plt.plot(x, cake, color)
+    plt.xlabel("x")
+    plt.ylabel("cake")
+    plt.title("Cake Profile")
+    
 
-#set model grid
-dx = 0.5 #step size
+D = 100 
+Lx = 300 
+
+dx = 0.5 
 x = np.arange(start=0, stop=Lx, step=dx)
 nx = len(x)
 
-#check value
-#extract 1 value
-x[1] #-1 will be the last value of array
-
-#extarct range
-x[0:5] #not including the last index so if want first 5 elements we need 5] not 4]
-
-#shortcut
-x[-5:] #last 5 
-
 C = np.zeros_like(x)
-
-#define 2 boundry conditions
 C_left = 500 
 C_right = 0
-
-#
 C[x <= Lx / 2] = C_left
 C[x > Lx / 2] = C_right
 
+plot_profile(x, C)
 
-plt.figure()
-plt.plot(x, C, "r")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("Initial Profile")
+nt = 5000 
+dt = 0.5 * dx ** 2 / D 
 
-
-nt = 5000 #time step
-dt = 0.5 * dx ** 2 / D ##dx^2
-
-
-for t in range(0, nt): #range from 0 to 4999, nt-1
+for t in range(0, nt): 
     C[1:-1] += D * dt / dx ** 2 * (C[:-2] - 2 * C[1:-1] + C[2:]) 
-#C[1:-1] 2nd value of variable C to last value 
-#+= updating the C[a,b] (leftside) by adding the rest (rightside of +=)
-#see formula
 
-
-plt.plot(x, C, "b")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("Final Profile")
-
+plot_profile(x, C, color="b")
